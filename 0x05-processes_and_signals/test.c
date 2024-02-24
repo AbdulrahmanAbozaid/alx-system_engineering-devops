9e28a9707b7906d2bb640bb2f186a6637221f61c
abdulrahmanabouzied <abdulrahman.ab.mahmoud@gmail.com>
2024-02-24 21:02:38 +0200

    processes

---
+++ 0x05-processes_and_signals/test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char **environ;
+
+void exitfunc(void)
+{
+    printf("\n Clean-up function called\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int count = 0;
+
+    atexit(exitfunc);
+
+    printf("\n");
+    while (environ[count++] != NULL)
+    {
+        // Dos some stuff
+    }
+
+    exit(0);
+    // _exit(0); // This will not call the atexit() function
+}
