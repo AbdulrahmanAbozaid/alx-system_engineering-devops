a09f21a787c43b80310d502dfb0f37032882174e
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-08-15 23:14:20 +0300

    [0x16.api_advanced]: Reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,10 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers (not active users,
       total subscribers) for a given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "request"}
     subreddits = requests.get(url, headers=headers, allow_redirects=False)
     if subreddits.status_code == 200:
         data = subreddits.json()
-        return data['data']['subscribers']
+        return data.get('data').get('subscribers')
     else:
         return 0
