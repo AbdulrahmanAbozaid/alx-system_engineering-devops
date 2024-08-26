4e51edf1826d9ee27baad43125070a4b44903c09
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-08-26 17:51:52 +0300

    [Starter]: getting started

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,10 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of subscribers (not active users,
       total subscribers) for a given subreddit"""
+    
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     headers = {'User-Agent': 'Mozilla/5.0 (compatible; MyRedditBot/0.1)'}
@@ -19,6 +23,6 @@ def number_of_subscribers(subreddit):
             return data['data']['subscribers']
         else:
             return 0
-    except requests.RequestException:
+    except Exception:
         # If there's an exception in the request, return 0
         return 0
