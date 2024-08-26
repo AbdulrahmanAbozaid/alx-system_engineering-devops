7f17474b236e7639eab206636e2fe386a9f3b25e
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-08-26 17:57:47 +0300

    [Starter]: getting started

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """How many subs?"""
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers (not active users,
       total subscribers) for a given subreddit"""
-
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
@@ -24,5 +22,4 @@ def number_of_subscribers(subreddit):
         else:
             return 0
     except Exception:
-        # If there's an exception in the request, return 0
         return 0
